3e46e4f279b5cf506d72904ebd0333bf3d99316a
shifttt <stepan.matusevich@yandex.com>
2021-07-21 21:11:43 -0400

    2nd

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -1,7 +1,11 @@
 package com.collection;
 
 public class Main {
-    public static void main(String[] args) {
-        for ()
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i<5; i++){
+            System.out.println("hej");
+        }
+
+        Thread.sleep(10000);
     }
 }
